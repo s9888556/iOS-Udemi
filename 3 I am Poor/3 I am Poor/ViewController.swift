1eb1cdd6e7da9c7a7266140ac35b9d8dc0ff5a82
Sergey <s9888556@yandex.ru>
2023-07-19 16:02:18 +0500

    Раздел 3 завершен

---
+++ 3 I am Poor/3 I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3 I am Poor
+//
+//  Created by Сергей П on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
