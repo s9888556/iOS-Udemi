83db87a46a168ccc7cb5ebd6b969c3b77074a91b
Sergey <s9888556@yandex.ru>
2023-07-28 20:00:26 +0500

    finish Section 7

--- 7 Xylophone/7 Xylophone/ViewController.swift
+++ 7 Xylophone/7 Xylophone/ViewController.swift
@@ -5,30 +5,6 @@
 //  Created by Сергей П on 27.07.2023.
 //
 
-//import UIKit
-//import AVFoundation
-//
-//class ViewController: UIViewController {
-//    
-//    var player: AVAudioPlayer!
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-//    
-//    @IBAction func keyPressed(_ sender: UIButton) {
-//        print(sender.currentTitle!)
-//        playSound(soundName: sender.currentTitle!)
-//    }
-//    
-//    func playSound(soundName: String) {
-//        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
-//        player = try! AVAudioPlayer(contentsOf: url!)
-//        player.play()
-//        
-//    }
-//}
-
 import UIKit
 import AVFoundation
 
@@ -38,22 +14,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        
+        print(sender.currentTitle!)
         playSound(soundName: sender.currentTitle!)
         
-        //Reduces the sender's (the button that got pressed) opacity to half.
         sender.alpha = 0.5
         
-        //Code should execute after 0.2 second delay.
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            //Bring's sender's opacity back up to fully opaque.
             sender.alpha = 1.0
         }
-        
     }
     
     func playSound(soundName: String) {
@@ -62,5 +33,4 @@ class ViewController: UIViewController {
         player.play()
         
     }
-    
 }
