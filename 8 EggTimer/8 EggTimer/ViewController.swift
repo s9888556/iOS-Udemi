87386d822334b5b75281bda16e600c3e5e576888
Sergey <s9888556@yandex.ru>
2023-07-29 11:28:35 +0500

    add 8 EggTimer app

---
+++ 8 EggTimer/8 EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8 EggTimer
+//
+//  Created by Сергей П on 29.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
