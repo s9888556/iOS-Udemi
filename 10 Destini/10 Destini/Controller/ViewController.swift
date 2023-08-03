1f80dba677625b1cd14af949438075f02dda1ec7
s9888556 <s9888556@yandex.ru>
2023-08-03 16:48:08 +0500

    finish Section 10

--- 10 Destini/10 Destini/Controller/ViewController.swift
+++ 10 Destini/10 Destini/Controller/ViewController.swift
@@ -12,12 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-        
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getAnswerChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getAnswerChoice2(), for: .normal)
+    }
 }
 
