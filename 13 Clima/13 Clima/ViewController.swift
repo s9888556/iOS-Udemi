c436b68e0e16f2d82fb364f85c31aa303f799c9e
s9888556 <s9888556@yandex.ru>
2023-08-18 16:58:24 +0500

    add 13 Clima app

---
+++ 13 Clima/13 Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  13 Clima
+//
+//  Created by Сергей П on 18.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
